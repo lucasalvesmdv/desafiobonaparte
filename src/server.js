2f950a64af4ce964019c13c7a42d41f935180dfd
Lucas Alves <lucasalvesmdv@gmail.com>
2022-04-04 20:52:29 -0300

    docker

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 import express from 'express';
-import mongoose from 'mongoose';
+//import mongoose from 'mongoose';
 import cors from 'cors'
 import rotes from '../routes/router.js'
 
 const app = express();
 
 
-mongoose.connect('mongodb://localhost:27017/bonaparte',) 
+//mongoose.connect('mongodb://localhost:27017/bonaparte',) 
 
-const db = mongoose.connection
-db.on('error', (err) => console.log(err))
-   db.once('open', () => console.log('Database Connected'))
+//const db = mongoose.connection
+//db.on('error', (err) => console.log(err))
+   //db.once('open', () => console.log('Database Connected'))
 
   
 
@@ -22,5 +22,5 @@ app.use(rotes)
 
 
 app.listen(3333, () => {
-  console.log('Server running!')
+  console.log('Server running!!!2')
 })
